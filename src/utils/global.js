a1406269e3553cdc67d3a1e74f76730262d4bd43
Aissa BOUGUERN <aissa.bouguern@gmail.com>
2019-10-23 14:21:51 -0400

    Fix Edit on Github Link

--- src/utils/global.js
+++ src/utils/global.js
@@ -2,17 +2,19 @@ import urljoin from 'url-join';
 import moment from 'moment';
 import config from '../../data/SiteConfig';
 
-import 'moment/locale/ar';
-moment.locale('ar');
+/* import 'moment/locale/ar';
+moment.locale('ar'); */
 
 const formatDate = date => moment.utc(date).format(config.dateFormat);
 
 const editOnGithub = post => {
   const date = moment.utc(post.date).format(config.dateFromFormat);
+  var slugArray = post.slug.split('/');
+
   return urljoin(
     config.repo,
     '/blob/master/content/posts',
-    `${date}-${post.slug}.md`
+    `${date}-${slugArray[slugArray.length - 1]}.md`
   );
 };
 
